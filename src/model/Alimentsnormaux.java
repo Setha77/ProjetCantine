2cd2d4ac721d5a30bd21e1c387778906c938cbd2
Setha Hong <setha_hong77@hotmail.com>
2022-03-08 16:15:25 +0100

    Co-authored-by: Gianny_Pnt <GiannyPontat@users.noreply.github.com> Co-authored-by: Younes A. <a6younes@users.noreply.github.com>

---
+++ src/model/Alimentsnormaux.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Alimentsnormaux extends Aliments {
+
+
+public Alimentsnormaux (String nomAlim , String allergene, int quantite){
+super(nomAlim,allergene,quantite);
+this.nomAlim = nomAlim;
+this.allergene = allergene;
+this.quantite = quantite;
+}
+
+
+}
